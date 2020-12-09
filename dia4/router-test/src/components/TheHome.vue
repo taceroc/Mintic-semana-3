9bbb3d278e472806d33fb097bdeaf6cd8b6cd818
taceroc <taceroc@unal.edu.co>
2020-12-09 08:07:51 -0500

    dia4, ver video para terminar lo de login

---
+++ dia4/router-test/src/components/TheHome.vue
@@ -0,0 +1,24 @@
+<template>
+<div  class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <p>Hola componente de home</p>
+</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    },
+    methods: {
+        getUserDetails(){
+
+        },
+        logOut(){
+            
+        }
+    }
+}
+</script>
