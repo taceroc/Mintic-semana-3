1b8b5a693500b85981cd2b3fd86fdba8a3f42296
taceroc <taceroc@unal.edu.co>
2020-12-11 18:37:00 -0500

    backend siguiendo guia

--- dia5ex/BackEnd/backend-login/controller/controller.js
+++ dia5ex/BackEnd/backend-login/controller/controller.js
@@ -2,32 +2,45 @@ const config = require('../secret/config.js');
 const db = require('../models');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const user = require('../models');
 
-exports.signin = (req, res) => {
-    db.user.findOne({
-        where: {
-            email: req.body.email
-        }
-    }).then(user => {
-        if (!user) {
-            return res.status(404).send('User Not Found.');
-        }
-        var passwordIsValid = bcrypt.compareSync(req.body.password,
-            user.password);
-        if (!passwordIsValid) {
-            return res.status(401).send({
-                auth: false, accessToken: null,
-                reason: "Invalid Password!"
-            });
-        }
-        var token = jwt.sign({
-            id: user.id, namame: user.namame, email:
-                user.email
-        }, config.secret, {
-            expiresIn: 86400 // expires in 24 hours
-        });
-        res.status(200).send({ auth: true, accessToken: token });
-    }).catch(err => {
-        res.status(500).send('Error -> ' + err);
-    });
-}
+exports.signin = async(req,res,next) =>{
+    try {
+        const user = await db.user.findOne({where: {email: req.body.email}});
+        console.log(req.body);
+        console.log(bcrypt.hashSync(req.body.password,8));
+        console.log(user.password);
+        //res.send(req.body.password);
+        if(user){
+            const passwordIsValid = await bcrypt.compare(req.body.password, user.password);
+            if(passwordIsValid){
+                const token = jwt.sign({
+                    id: user.id,
+                    name: user.namame,
+                    email: user.email,
+                }, 'config.secret',{
+                    expiresIn: 86400, //en segundos
+                }
+                );
+                res.status(200).send({
+                    auth: true,
+                    accessToken: token,
+                    user: user
+                })
+            }else{
+                res.status(401).json({
+                    error: 'Error en el usuario o contraseña'
+                })
+            };
+        }else{
+            res.status(404).json({
+                error: 'Error email'
+            })
+        };
+    } catch (error) {
+        res.status(500).send({
+            message: 'Error ->'
+        })
+        next(error);
+    }
+};
--- dia5ex/BackEnd/backend-login/seeders/20201211023329-demo-user.js
+++ dia5ex/BackEnd/backend-login/seeders/20201211023329-demo-user.js
@@ -1,17 +1,19 @@
 'use strict';
+const bcrypt = require('bcryptjs');
+const models = require('../models');
+
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [{
       namame: 'carlos',
       email: 'ejemplo@gmail.com',
-      password:
-        '$2y$08$UJL1Cx9ELiFDZKgfnSURxuOf/uv7WqpxVTu3Q6vpA.HKVOkwq4bIm',
+      password: "$2y$08$W5uvFfIaCMzRDne1QOMzvut0vIPTYVoq4t9LxBAdKahTlBnAdEqDi",
       createdAt: new Date(),
       updatedAt: new Date()
-    }]);
+    }], {});
   },
-  down: async (queryInterface, Sequelize) => {
+  down: async (queryInterface) => {
     return queryInterface.bulkDelete('Users', null, {});
   }
 };
