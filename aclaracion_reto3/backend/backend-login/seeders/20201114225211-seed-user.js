224a3f4eed6f17083d6b383341327d8a3420dec8
taceroc <taceroc@unal.edu.co>
2020-12-12 16:25:03 -0500

    backend proyecto final

--- aclaracion_reto3/backend/backend-login/seeders/20201114225211-seed-user.js
+++ aclaracion_reto3/backend/backend-login/seeders/20201114225211-seed-user.js
@@ -2,17 +2,17 @@
 
 module.exports = {
     up: async(queryInterface, Sequelize) => {
-        return queryInterface.bulkInsert('Users', [{
+        return queryInterface.bulkInsert('users', [{
             name: 'carlos',
             email: 'ejemplo@gmail.com',
-            password: '$2y$08$FTP/jKGNASwJf0ero7SBe.kQmUsOSjWYupPZ6/lS6en6RcithXFKO',
+            password: '$2y$12$qImULeQqTta9YKkkrzgmY.OZpX2pdUOU3xblJIbZEXjhaMsd3Yi4u',//micontraseña
             createdAt: new Date(),
             updatedAt: new Date()
         }]);
     },
 
     down: async(queryInterface, Sequelize) => {
-        return queryInterface.bulkDelete('Users', null, {});
+        return queryInterface.bulkDelete('users', null, {});
 
     }
 };
--- aclaracion_reto3/backend/backend-login/server.js
+++ aclaracion_reto3/backend/backend-login/server.js
@@ -2,31 +2,52 @@
 debe importar como se observa en la siguiente linea, con el nombre del archivo js
 que contiene la logica */
 //const controller = require('./controller/nombredelcontrollador.js');
-const express = require('express');
-const db = require('./models');
-const app = express()
-const bodyParser = require('body-parser');
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+const express = require("express");
+const morgan = require("morgan");
+const apiRouter = require("./routes");
+const bodyParser = require ("body-parser");
+const cors = require('cors');
+
+
+
+//instancia de express en mi app
+const app = express();
+app.use(cors());
+
+app.use((req, res, next)=>{
+    res.header("Acces-Control-Allow-Original", "*");
+    res.header("Acces-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Acces-Control-Allow-Methods: GET, POST, DELETE");
     next();
-});
+})
+
+//middleware morgan para detectar peticiones
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }));
+
+
+//manejador rutas
+
+app.use('/api', apiRouter);
+
+app.set('PORT', process.env.PORT || 3000);
+
 
 // API ENDPOINTS
 /*se debe contar un una ruta por medio de método post para el inicio de sesión de la siguiente manera:
 '/api/auth/signin'
 */
-app.get('/', function(req, res) {
+//primera ruta
+app.get('/', (req, res) => {
     console.log("Estructura base del proyecto backend");
     res.send("Estructura base del proyecto backend");
 });
-const port = 3000
-app.listen(port, () => {
-    console.log(`Running on http://localhost:${port}`)
+
+app.listen(app.get('PORT'), () => {
+    console.log(`Running on http://localhost:${app.get('PORT')}`)
 })
 
 module.exports = app;
